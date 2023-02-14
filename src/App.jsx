82c905bc8eb84ff69d92df12d52066b1097a03e3
vivinkv6 <vivinkv0@gmail.com>
2023-02-14 12:21:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1>Hand Cricket</h1>
+    </div>
+  )
+}
+
+export default App
